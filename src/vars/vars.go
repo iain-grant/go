2cae121d67f66175816cac8bb0f1b4e30251eb76
Iain Grant <iain.x.grant@gmail.com>
2021-09-07 12:43:57 +0100

    Variables example

---
+++ src/vars/vars.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 100
+	pi := 3.1415926536
+
+	fmt.Printf("num: %v type:%T \n", num, num)
+	fmt.Printf("pi: %v type:%T \n\n", pi, pi)
+
+	fmt.Printf("%%7d displays %7d \n", num)
+	fmt.Printf("%%07d displays %07d \n\n", num)
+
+	fmt.Printf("Pi is appromimately %1.10f \n", pi)
+	fmt.Printf("Right-aligned %20.3f rounded pi", pi)
+	fmt.Printf("Left-aligned %-20.3f rounded pi", pi)
+}
