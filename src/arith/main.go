18a3b3f7ad3f0a0851dad90f65384a60b8cb0a92
Iain Grant <iain.x.grant@gmail.com>
2021-09-08 20:10:48 +0100

    Arithmetic examples

---
+++ src/arith/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 8
+	b := 4
+
+	fmt.Println("Addition:\t", (a + b))
+	fmt.Println("Subtraction:\t", (a - b))
+	fmt.Println("Multiplication:\t", (a * b))
+	fmt.Println("Division:\t", (a / b))
+	fmt.Println("Remainder:\t", (a % b))
+
+	a++
+	fmt.Println("Increment:\t", a)
+	b--
+	fmt.Println("Decrement:\t", b)
+}
